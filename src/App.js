9e6bdd4c20f1d892772dd8f87b9c00dd56145074
Angel Gonzalez <58734248+MeBeAngel@users.noreply.github.com>
2022-07-13 15:05:22 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.scss";
+import ProfileImg from "./components/ProfileImg/ProfileImg";
+import MenuSection from './components/MenuSection/MenuSection';
+
+export default function App() {
+  return (
+    <div className="App">
+      <ProfileImg />
+      <MenuSection />
+    </div>
+  );
+}
---
+++ src/components/MenuSection/MenuSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default function MenuSection() {
+  return (
+    <div>
+      <p>Menu Item</p>
+      <p>Menu Item</p>
+      <p>Menu Item</p>
+    </div>
+  )
+}
