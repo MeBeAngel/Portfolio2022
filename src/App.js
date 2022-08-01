4c81b228c23355b721c82b1a12f55dacadbfa6c4
Angel Gonzalez <58734248+MeBeAngel@users.noreply.github.com>
2022-08-01 09:54:24 -0400

    Add CSS Vendor Prefixes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-//import "bootstrap-icons/font/bootstrap-icons.css";
-import "./styles.scss";
+import "./styles.css";
 import GameBoy from "./components/GameBoy/GameBoy";
 
 export default function App() {
@@ -8,8 +7,6 @@ export default function App() {
   return (
     <main className="App">
       <GameBoy />
-      {/* <h1>Sorry I missed you!</h1>
-      <h1>Under maintenance. Will be back up shortly!</h1> */}
     </main>
   );
 }
--- src/components/GameBoy/CornerVent.jsx
+++ src/components/GameBoy/CornerVent.jsx
@@ -4,8 +4,8 @@ import React from 'react';
 export default function CornerVent() {
     return (
         <>
-        <div className="vent-bar vent-bar-1"></div>
-        <div className="vent-bar vent-bar-2"></div>
+            <div className="vent-bar vent-bar-1"></div>
+            <div className="vent-bar vent-bar-2"></div>
             <div className="vent-bar vent-bar-3"></div>
             <div className="vent-bar vent-bar-4"></div>
         </>
